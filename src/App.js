9e44a166a9b4f96f1d0c3451285708948d7067c6
Niilo Elias Räisänen <s4rani02@students.osao.fi>
2024-08-16 13:24:45 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ function App() {
     <main className="background">
       <div className="top-line"></div>
       
-      <div className="top-line"></div>
+      <div className="top-text">Raiskale.com</div>
       
+
+
     </main>
     
   );
